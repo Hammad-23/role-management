af3eb5ab3075850b43f4b9b1063b5260383e3cde
Hammad-23 <hsyed2633@gmail.com>
2020-11-06 16:03:42 +0500

    done

--- src/vies/employeeform/index.js
+++ src/vies/employeeform/index.js
@@ -1,10 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from "react-router-dom";
 import BasicTextFields from '../../components/input'
 import CustomButton from '../../components/button'
 
 function EmployeeForm(){
     let history = useHistory();
+    const [profileName,setName]=useState('')
+    const [education,setEdu]=useState('')
+    const [degree,setDegree]=useState('')
+    const [school,setSchool]=useState('')
+    const [num,setNum]=useState('')
+    const [adress,setAdress]=useState('')
+    const [age,setAge]=useState('')
+
+    const fullName=(e)=>{
+      const val=e.target.value
+      setName(val)
+
+    }
+
+    const highestEducation=(e)=>{
+     const val = e.target.value
+     setEdu(val)
+
+    }
+
+    const lastDegree=(e)=>{
+     const val = e.target.value
+     setDegree(val)
+
+    }
+
+    const lastSchool=(e)=>{
+        const val = e.target.value
+        setSchool(val)
+   
+       }
+
+    const number=(e)=>{
+        const val = e.target.value
+        setNum(val)
+   
+       }
+
+    const currentAdress=(e)=>{
+        const val = e.target.value
+        setAdress(val)
+   
+       }
+
+    const yourAge=(e)=>{
+        const val = e.target.value
+        setAge(val)
+   
+       }
+
+       const profileInfo={
+           profileName,
+           education,
+           degree,
+           school,
+           num,
+           adress,
+           age
+       }
+
+    const submit=()=>{
+        localStorage.setItem('info',profileInfo)
+        alert('Your Information have been saved,Thankyou')
+        history.push('/dashboardemployee')
+    }   
+    
     return(
         <>
         <div style={{backgroundColor:'#01064f',height:'4rem'}}>
@@ -12,35 +78,35 @@ function EmployeeForm(){
         </div>
 
         <div>
-            <BasicTextFields id='standard-basic' label='Enter Your Full Name' />
+            <BasicTextFields onChange={fullName} id='standard-basic' label='Enter Your Full Name' />
 
         </div>
         <div>
-            <BasicTextFields id='standard-basic' label='Enter highest Education' />
+            <BasicTextFields onChange={highestEducation} id='standard-basic' label='Enter highest Education' />
 
         </div>
         <div>
-            <BasicTextFields id='standard-basic' label='Enter Last Degree Name' />
+            <BasicTextFields onChange={lastDegree} id='standard-basic' label='Enter Last Degree Name' />
 
         </div>
         <div>
-            <BasicTextFields id='standard-basic' label='Enter Last School Name' />
+            <BasicTextFields onChange={lastSchool} id='standard-basic' label='Enter Last School Name' />
 
         </div>
         <div>
-            <BasicTextFields id='standard-basic' label='Enter Mobile Number' type='number' />
+            <BasicTextFields onChange={number} id='standard-basic' label='Enter Mobile Number' type='number' />
 
         </div>
         <div>
-            <BasicTextFields id='standard-basic' label='Enter Your Current adress' />
+            <BasicTextFields onChange={currentAdress} id='standard-basic' label='Enter Your Current adress' />
 
         </div>
         <div>
-            <BasicTextFields id='standard-basic' label='Enter Your Age' type='number' />
+            <BasicTextFields onChange={yourAge} id='standard-basic' label='Enter Your Age' type='number' />
 
         </div>
         <div>
-            <CustomButton onClick={()=>{history.push('/dashboardemployee')}} text='Submit' variant='contained' color='primary'/>
+            <CustomButton onClick={submit} text='Submit' variant='contained' color='primary'/>
 
         </div>
 
