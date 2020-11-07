8d6a14edeac6232698c9e62e69e1dd5a331dea34
Hammad-23 <hsyed2633@gmail.com>
2020-11-08 01:31:41 +0500

    buttons rendering

--- src/vies/dashboardemployee/index.js
+++ src/vies/dashboardemployee/index.js
@@ -3,10 +3,12 @@ import { useHistory } from "react-router-dom";
 import CustomButton from "../../components/button";
 import FloatingActionButtons from "../../components/floatingbutton";
 import ButtonAppBar from "../../components/navbar";
+import FloatingEditButton from '../../components/floatingedit'
 
 import { getData } from "../../config/firebase";
 
 function DashboardEmployee() {
+    
     const [show,setShow]=useState([])
   let history = useHistory();
   useEffect(() => {
@@ -24,16 +26,36 @@ fetchData()
 
   }, []);
 console.log(show)
+const [see,setSee]=useState(true)
+const [float,setFloat]=useState(true)
+// if(show!==null){
+//   setSee(false)
+// }else{
+//   setSee(true)
+// }
+useEffect(()=>{
+  if(show.length===null){
+    console.log('khali hai')
+   setSee(true)
+   setFloat(false)
+  }else{
+    console.log('full hai')
+   setSee(false)
+   setFloat(true)
+  }
+},[])
+
   return (
     <>
       <ButtonAppBar text="Dashboard Of Employee" title="Log Out" />
 
-      <FloatingActionButtons
+    { see&& <FloatingActionButtons
         onClick={() => {
           history.push("/employeeform");
         }}
         color="primary"
-      />
+      /> }
+    { float&& <FloatingEditButton/> }
     </>
   );
 }
